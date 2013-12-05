5944a9bee52ca8c1b6a0ed94535f7e9ccb9e5c47
Ben Kurtovic <ben.kurtovic@gmail.com>
2013-12-04 23:17:45 -0500

    Edit example too.

--- ben.c
+++ ben.c
@@ -105,6 +105,43 @@ void ex3_new() {
     printf(XT_CH_NORMAL);
 }
 
+void ex4_edit() {
+    printf(XT_CH_INVERSE);
+    printf(" MYSTORE :: PBrooks :: Version 0.90                                   5 Records \n");
+    printf(XT_CH_NORMAL);
+    printf("[W/S] Scroll   [N] New   [F] Find   [G] Go To         First: 2013-11-04 20:36:55\n");
+    printf("[Enter] Edit             [H] Help   [Q] Quit           Last: 2013-11-04 21:10:10\n");
+    printf("\n");
+    printf("   ID SUBJECT                                                TIME               \n");
+    printf("    1 Hello, world!                                          2013-11-04 20:36:55\n");
+    printf("->  2 Lorem ipsum dolor sit amet                             2013-11-04 20:43:04\n");
+    printf("        Lorem ipsum dolor sit amet, consectetur adipisicing\n");
+    printf("        elit, sed do eiusmod tempor incididunt ut labore et\n");
+    printf("        dolore magna aliqua.\n");
+    printf("    3 Another Entry                                          2013-11-04 20:46:25\n");
+    printf("    4 A Fourth Entry                                         2013-11-04 20:46:29\n");
+    printf("    5 Entry Five                                             2013-11-04 21:10:10\n");
+    xt_par2(XT_SET_ROW_COL_POS, 6, 11);
+    printf(XT_CH_INVERSE);
+    printf("                          EDIT ENTRY                          \n");
+    xt_par2(XT_SET_ROW_COL_POS, 7, 11);
+    printf(" %s                                                            %s \n", XT_CH_NORMAL, XT_CH_INVERSE);
+    xt_par2(XT_SET_ROW_COL_POS, 8, 11);
+    printf(" %s  %sSubject:%s %sLorem ipsum %s%sd%s%solor sit amet               %s        %s \n", XT_CH_NORMAL, XT_CH_YELLOW, XT_CH_DEFAULT, XT_CH_UNDERLINE, XT_CH_NORMAL, XT_CH_INVERSE, XT_CH_NORMAL, XT_CH_UNDERLINE, XT_CH_NORMAL, XT_CH_INVERSE);
+    xt_par2(XT_SET_ROW_COL_POS, 9, 11);
+    printf(" %s   %sBody:%s   %sLorem ipsum dolor sit amet, consectetur adipis%s   %s \n", XT_CH_NORMAL, XT_CH_YELLOW, XT_CH_DEFAULT, XT_CH_UNDERLINE, XT_CH_NORMAL, XT_CH_INVERSE);
+    xt_par2(XT_SET_ROW_COL_POS, 10, 11);
+    printf(" %s           %sicing elit, sed do eiusmod tempor incididunt u%s   %s \n", XT_CH_NORMAL, XT_CH_UNDERLINE, XT_CH_NORMAL, XT_CH_INVERSE);
+    xt_par2(XT_SET_ROW_COL_POS, 11, 11);
+    printf(" %s           %st labore et dolore magna aliqua.              %s   %s \n", XT_CH_NORMAL, XT_CH_UNDERLINE, XT_CH_NORMAL, XT_CH_INVERSE);
+    xt_par2(XT_SET_ROW_COL_POS, 12, 11);
+    printf(" %s                                                            %s \n", XT_CH_NORMAL, XT_CH_INVERSE);
+    xt_par2(XT_SET_ROW_COL_POS, 13, 11);
+    printf(" %s     %s[Arrow Keys]%s Navigate   %s[Enter]%s Save   %s[F5]%s Cancel     %s \n", XT_CH_NORMAL, KEY_COLOR, KEY_COLOR, KEY_COLOR, XT_CH_INVERSE);
+    xt_par2(XT_SET_ROW_COL_POS, 14, 11);
+    printf("                                                              \n");
+    printf(XT_CH_NORMAL);
+}
 int main() {
     xt_par0(XT_CLEAR_SCREEN);
     xt_par2(XT_SET_ROW_COL_POS, 1, 1);
@@ -113,10 +150,11 @@ int main() {
     ex2_scrolling();
     PREPARE()
     ex3_new();
-    // ex3_edit();
-    // ex3_find();
-    // ex3_goto();
-    // ex3_help();
+    PREPARE()
+    ex4_edit();
+    // ex5_find();
+    // ex6_goto();
+    // ex7_help();
     xt_par2(XT_SET_ROW_COL_POS, 25, 1);
     return 0;
 }
